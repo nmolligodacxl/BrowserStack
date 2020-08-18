1a841a4b3b68de680f6530596d63232fbcd3cca2
nmolligodacxl <62119339+nmolligodacxl@users.noreply.github.com>
2020-08-18 17:07:42 -0400

    new repo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+//using OpenQA.Selenium.Appium;
+//using OpenQA.Selenium.Appium.Android;
+//using OpenQA.Selenium.Remote;
+//using OpenQA.Selenium.Support.UI;
+//using System.Collections.Generic;
+
+//namespace BrowserStackAppiumSingleTest
+//{
+//    class MainClass
+//    {
+//        readonly static string userName = "navkunjkumar1";
+//        readonly static string accessKey = "AjFHUwkUxk7rhTprqBmE";
+
+//        public static void Main(string[] args)
+//        {
+//            //DesiredCapabilities caps = new DesiredCapabilities();
+//            AppiumOptions caps = new AppiumOptions();
+
+//            caps.AddAdditionalCapability("browserstack.user", userName);
+//            caps.AddAdditionalCapability("browserstack.key", accessKey);
+//            caps.AddAdditionalCapability("device", "Google Pixel 3");
+//            caps.AddAdditionalCapability("os_version", "9.0");
+//            caps.AddAdditionalCapability("project", "My First Project");
+//            caps.AddAdditionalCapability("build", "My First Build");
+//            caps.AddAdditionalCapability("name", "Bstack-[C#] Sample Test");
+//            caps.AddAdditionalCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+
+//            AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new Uri("http://hub-cloud.browserstack.com/wd/hub"), caps);
+//            AndroidElement searchElement = (AndroidElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+//                ExpectedConditions.ElementToBeClickable(MobileBy.AccessibilityId("Search Wikipedia"))
+//            );
+//            searchElement.Click();
+//            AndroidElement insertTextElement = (AndroidElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+//                ExpectedConditions.ElementToBeClickable(MobileBy.Id("org.wikipedia.alpha:id/search_src_text"))
+//            );
+//            insertTextElement.SendKeys("BrowserStack");
+//            System.Threading.Thread.Sleep(5000);
+
+//            IReadOnlyList<AndroidElement> allTextViewElements = driver.FindElementsByClassName("android.widget.TextView");
+//            Console.WriteLine(allTextViewElements.Count > 0);
+
+//            driver.Quit();
+//        }
+//    }
+//}
---
+++ ConsoleApp1/Program2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Remote;
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+            //DesiredCapabilities capability = new DesiredCapabilities();
+            string cloudHost = "BrowserStack";
+            //string cloudHost = "LambdaTest";
+            AppiumOptions capability = new AppiumOptions();
+
+            if (cloudHost.Equals("BrowserStack"))
+            {
+                Console.WriteLine("Browserstack..");
+                capability.AddAdditionalCapability("browserName", "Android");
+                capability.AddAdditionalCapability("device", "Google Pixel 3");
+                capability.AddAdditionalCapability("os_version", "10.0");
+                //capability.AddAdditionalCapability("browserName", "Safari");
+                //capability.AddAdditionalCapability("device", "iPhone 11");
+                //capability.AddAdditionalCapability("os_version", "14");
+                capability.AddAdditionalCapability("realMobile", "true");
+                capability.AddAdditionalCapability("project", "Android/IOS Brrowser Project");
+                capability.AddAdditionalCapability("build", "Test Google Build");
+                capability.AddAdditionalCapability("browserstack.user", "navkunjkumar1");
+                capability.AddAdditionalCapability("browserstack.key", "AjFHUwkUxk7rhTprqBmE");
+                driver = new RemoteWebDriver(
+                  new Uri("https://hub-cloud.browserstack.com/wd/hub/"), capability );
+
+            } else {
+
+            Console.WriteLine("LambdaTest..");
+            capability.AddAdditionalCapability("build", "Test Google Build");
+            capability.AddAdditionalCapability("name", "Android/IOS Brrowser Project");
+            capability.AddAdditionalCapability("platformName", "Android");
+            capability.AddAdditionalCapability("deviceName", "Google Pixel 3");
+            capability.AddAdditionalCapability("platformVersion", "10");
+            //capability.AddAdditionalCapability("platformName", "iOS");
+            //capability.AddAdditionalCapability("deviceName", "iPad (6th generation)");
+            //capability.AddAdditionalCapability("platformVersion", "13.1");
+            driver = new RemoteWebDriver(
+              new Uri("https://agore:kDsO40zMp5T8Xa6OsXeJUk0JORXUeReqOKZ3bUaWdxUM7qyfq7@hub.lambdatest.com/wd/hub"), capability);
+            }
+
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Console.WriteLine(driver.Title);
+            IWebElement txtSearch = driver.FindElement(By.Name("q"));
+            txtSearch.SendKeys("Disney world");
+            txtSearch.Submit();
+
+            Console.WriteLine(driver.Title);
+            driver.Quit();
+        }
+    }
+}
